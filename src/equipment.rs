888f791e6acf70037d6f57bdf17f80a8b1dfbdbf
lenny <cocoblackett@gmail.com>
2026-06-20 13:20:26 +0200

    equipement fn

--- src/equipment.rs
+++ src/equipment.rs
@@ -1,9 +1,10 @@
-use crate::item::Item;
+use crate::item::{EquipmentSlot, Item};
 
 pub struct Equipment {
     pub weapon: Option<Item>,
     pub armor: Option<Item>,
     pub shield: Option<Item>,
+    pub consumables: Vec<Item>,
 }
 
 impl Equipment {
@@ -12,12 +13,36 @@ impl Equipment {
             weapon: None,
             armor: None,
             shield: None,
+            consumables: Vec::new(),
         }
     }
 
-    // TODO: équiper un item dans le bon slot
-    // pub fn equip(&mut self, item: Item) { ... }
+    pub fn equip(&mut self, item: Item) {
+        match item {
+            Item::Weapon(_) => self.weapon = Some(item),
+            Item::Armor(_) => self.armor = Some(item),
+            Item::Shield(_) => self.shield = Some(item),
+            Item::Consumable(_) => {
+                if self.consumables.len() < 5 {
+                    self.consumables.push(item);
+                }
+            }
+        }
+    }
 
-    // TODO: déséquiper un slot
-    // pub fn unequip(&mut self, slot: EquipmentSlot) -> Option<Item> { ... }
+    pub fn unequip(&mut self, slot: EquipmentSlot) -> Option<Item> {
+        match slot {
+            EquipmentSlot::Weapon => self.weapon.take(),
+            EquipmentSlot::Armor => self.armor.take(),
+            EquipmentSlot::Shield => self.shield.take(),
+        }
+    }
+
+    pub fn unequip_consumable(&mut self, index: usize) -> Option<Item> {
+        if index < self.consumables.len() {
+            Some(self.consumables.remove(index))
+        } else {
+            None
+        }
+    }
 }
